98a1f6ba378f521ac8956770876f22bc6fb62cf8
dlarudgus20 <dlarudgus20@naver.com>
2018-03-29 02:39:06 +0000

    2-1 typeinfo

--- 2-variable/1-literal.cpp
+++ 2-variable/1-literal.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <string>
+#include <typeinfo>
 
 int main()
 {
-    short x = 3ULL; 
+    using namespace std::literals;
+
+    auto x = 3ULL;
+    if (typeid(x) == typeid(unsigned long long))
+        std::cout << "unsigned long long\n";
+
+    auto y = "string"s;
+    if (typeid(y) == typeid(std::string))
+        std::cout << "std::string\n";
 }
 
